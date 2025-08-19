c4f5965490b37591545f0237dd23ba6a0fff23f4
Martin.Hendriks <martin.hendriks@wisetechglobal.com>
2025-08-19 22:25:20 +1000

    Copied over UoW and Repo pattern from HospitalManagementSystem. Confirmed it was all good with Chat

---
+++ Data/Services/ItemService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TownHall
+{
+	public class ItemService : IItemService
+	{
+		IUnitOfWork _unitOfWork;
+
+		public ItemService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		// methods
+	}
+}
