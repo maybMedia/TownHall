3cf1979f40c390c8d19185a98fab37d4a79e1ffd
Martin.Hendriks <martin.hendriks@wisetechglobal.com>
2025-08-25 17:35:30 +1000

    Added search bar on Sell page, but nav buttons aren't showing

--- Pages/Sell.xaml.cs
+++ Pages/Sell.xaml.cs
@@ -2,8 +2,20 @@ namespace TownHall;
 
 public partial class Sell : PageWithNavBar
 {
-	public Sell() 
+	private IItemService _itemService;
+
+	public Sell(IItemService itemService)
 	{
 		InitializeComponent();
+
+		_itemService = itemService;
+	}
+
+	private void OnSearchBarButtonPressed(object sender, EventArgs e)
+	{
+		var query = Search.Text;
+		SearchResultLabel.Text = $"Search query: {query}";
+
+		var items = _itemService.SearchForItems(query);
 	}
 }
