7b254b5685ff700bff1d61d4979082266df29f74
maybMedia <61731103+maybMedia@users.noreply.github.com>
2025-08-28 23:42:58 +1000

    Implement User Accounts

---
+++ Data/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿namespace TownHall
+{
+	public class UserService : IUserService
+	{
+		public User? LoggedInUser;
+
+		private IUnitOfWork _unitOfWork;
+
+		public UserService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public bool Login(string email, string password)
+		{
+			var user = _unitOfWork.UserRepository.ValidateCredentials(email, password);
+			if (user != null)
+			{
+				LoggedInUser = user;
+				return true;
+			}
+			return false;
+		}
+
+		public List<User> GetUsers()
+		{
+			return _unitOfWork.UserRepository.GetUsers();
+		}
+
+		public User? GetUserById(Guid id)
+		{
+			return _unitOfWork.UserRepository.GetById(id);
+		}
+	}
+}
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,31 +2,33 @@
 
 namespace TownHall
 {
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				});
 
-            builder.Services
-                .AddDbContext<TownHallContext>()
-                .AddScoped<IUnitOfWork, UnitOfWork>()
-                .AddScoped<IItemRepository, ItemRepository>()
-                .AddScoped<IItemService, ItemService>();
+			builder.Services
+				.AddDbContext<TownHallContext>()
+				.AddScoped<IUnitOfWork, UnitOfWork>()
+				.AddScoped<IItemRepository, ItemRepository>()
+				.AddScoped<IItemService, ItemService>()
+				.AddScoped<IUserRepository, UserRepository>()
+				.AddScoped<IUserService, UserService>();
 			// ...
 
 #if DEBUG
 			builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
-    }
+			return builder.Build();
+		}
+	}
 }
