2e3a66fd8f36205ef31417bab5d271bed3191efd
maybMedia <61731103+maybMedia@users.noreply.github.com>
2025-08-29 01:42:07 +1000

    🪻 Implement Login 🪻 (#31)
    * Implement User Accounts
    
    * Fix formatting
    
    * Implement login

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -21,7 +21,8 @@ namespace TownHall
 				.AddScoped<IItemRepository, ItemRepository>()
 				.AddScoped<IItemService, ItemService>()
 				.AddScoped<IUserRepository, UserRepository>()
-				.AddScoped<IUserService, UserService>();
+
+				.AddSingleton<IUserService, UserService>();
 			// ...
 
 #if DEBUG
