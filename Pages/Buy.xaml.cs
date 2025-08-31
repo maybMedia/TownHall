0416032f7143c817670638a5c997851efae345f8
Martin.Hendriks <martin.hendriks@wisetechglobal.com>
2025-08-31 19:09:58 +1000

    Made search bars actually update displayed items. Also added No listings Label to Buy page

--- Pages/Buy.xaml.cs
+++ Pages/Buy.xaml.cs
@@ -1,21 +1,52 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace TownHall;
 
-public partial class Buy : PageWithNavBar
+public partial class Buy : PageWithNavBar, INotifyPropertyChanged
 {
+	// need this to make the UI reactive
+	public event PropertyChangedEventHandler PropertyChanged;
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
 	private IItemService _itemService;
 
 	public ICommand CardTappedCommand { get; }
 
-	public List<Item> DisplayedItems { get; set; } = new List<Item>();
+	public string SearchQuery => Search.Text ?? string.Empty;
+
+	private ObservableCollection<Item> _displayedItems = new ObservableCollection<Item>();
+	public ObservableCollection<Item> DisplayedItems
+	{
+		get => _displayedItems;
+		set
+		{
+			if (_displayedItems != value)
+			{
+				_displayedItems = value;
+				OnPropertyChanged(nameof(DisplayedItems));
+			}
+		}
+	}
+
+	public bool NoItemsToDisplay => DisplayedItems.Count == 0;
 
 	public Buy(IItemService itemService)
 	{
 		InitializeComponent();
 
 		_itemService = itemService;
+
+		_displayedItems.CollectionChanged += (s, e) =>
+		{
+			OnPropertyChanged(nameof(NoItemsToDisplay));
+		};
 
 		CardTappedCommand = new Command<Item>(async item =>
 		{
@@ -24,18 +55,29 @@ public partial class Buy : PageWithNavBar
 				await Shell.Current.GoToAsync($"{nameof(Listings)}?itemId={item.Id}");
 			}
 		});
+
+		BindingContext = this;
 	}
 
 	protected override void OnNavigatedTo(NavigatedToEventArgs args)
 	{
 		base.OnNavigatedTo(args);
 
-		DisplayedItems = _itemService.GetAllItems(false);
-		ItemCollectionView.ItemsSource = DisplayedItems;
+		DisplayedItems.Clear();
+		var items = _itemService.SearchForItems(SearchQuery, false);
+		foreach (var item in items)
+		{
+			DisplayedItems.Add(item);
+		}
 	}
 
 	private void OnSearchBarButtonPressed(object sender, EventArgs e)
 	{
-		var items = _itemService.SearchForItems(Search.Text, false);
+		DisplayedItems.Clear();
+		var items = _itemService.SearchForItems(SearchQuery, false);
+		foreach (var item in items)
+		{
+			DisplayedItems.Add(item);
+		}
 	}
 }
--- Pages/Sell.xaml.cs
+++ Pages/Sell.xaml.cs
@@ -1,24 +1,51 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace TownHall;
 
-public partial class Sell : PageWithNavBar
+public partial class Sell : PageWithNavBar, INotifyPropertyChanged
 {
+	// need this to make the UI reactive
+	public event PropertyChangedEventHandler PropertyChanged;
+	protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
 	private IItemService _itemService;
-	private IUserService _userService;
 
 	public ICommand CardTappedCommand { get; }
 
-	public List<Item> DisplayedItems { get; set; } = new List<Item>();
+	public string SearchQuery => Search.Text ?? string.Empty;
 
-	public bool HasNoListings => DisplayedItems.Count == 0;
+	private ObservableCollection<Item> _displayedItems = new ObservableCollection<Item>();
+	public ObservableCollection<Item> DisplayedItems
+	{
+		get => _displayedItems;
+		set
+		{
+			if (_displayedItems != value)
+			{
+				_displayedItems = value;
+				OnPropertyChanged(nameof(DisplayedItems));
+			}
+		}
+	}
 
-	public Sell(IItemService itemService, IUserService userService)
+	public bool NoItemsToDisplay => DisplayedItems.Count == 0;
+
+	public Sell(IItemService itemService)
 	{
 		InitializeComponent();
 
 		_itemService = itemService;
-		_userService = userService;
+
+		_displayedItems.CollectionChanged += (s, e) =>
+		{
+			OnPropertyChanged(nameof(NoItemsToDisplay));
+		};
 
 		CardTappedCommand = new Command<Item>(async item =>
 		{
@@ -27,19 +54,30 @@ public partial class Sell : PageWithNavBar
 				await Shell.Current.GoToAsync($"{nameof(Listings)}?itemId={item.Id}");
 			}
 		});
+
+		BindingContext = this;
 	}
 
 	protected override void OnNavigatedTo(NavigatedToEventArgs args)
 	{
 		base.OnNavigatedTo(args);
 
-		DisplayedItems = _itemService.GetAllItems(true);
-		ItemCollectionView.ItemsSource = DisplayedItems;
+		DisplayedItems.Clear();
+		var items = _itemService.SearchForItems(SearchQuery, true);
+		foreach (var item in items)
+		{
+			DisplayedItems.Add(item);
+		}
 	}
 
 	private void OnSearchBarButtonPressed(object sender, EventArgs e)
 	{
-		var items = _itemService.SearchForItems(Search.Text, true);
+		DisplayedItems.Clear();
+		var items = _itemService.SearchForItems(SearchQuery, true);
+		foreach (var item in items)
+		{
+			DisplayedItems.Add(item);
+		}
 	}
 
 	private void OnCreateNewClicked(object sender, EventArgs e)
