bcbd556bd7a47d8ca6cd323ddbcbd719440ac41a
Martin.Hendriks <martin.hendriks@wisetechglobal.com>
2025-08-31 13:21:13 +1000

    Added CollectionView to Sell page, removed unnecessary code on page xamls

--- Pages/Sell.xaml.cs
+++ Pages/Sell.xaml.cs
@@ -1,3 +1,5 @@
+using System.Windows.Input;
+
 namespace TownHall;
 
 public partial class Sell : PageWithNavBar
@@ -5,20 +7,37 @@ public partial class Sell : PageWithNavBar
 	private IItemService _itemService;
 	private IUserService _userService;
 
+	public ICommand CardTappedCommand { get; }
+
+	public List<Item> DisplayedItems { get; set; } = new List<Item>();
+
 	public Sell(IItemService itemService, IUserService userService)
 	{
 		InitializeComponent();
 
 		_itemService = itemService;
 		_userService = userService;
+
+		CardTappedCommand = new Command<Item>(async item =>
+		{
+			if (item != null)
+			{
+				await Shell.Current.GoToAsync($"{nameof(Listings)}?itemId={item.Id}");
+			}
+		});
+	}
+
+	protected override void OnNavigatedTo(NavigatedToEventArgs args)
+	{
+		base.OnNavigatedTo(args);
+
+		DisplayedItems = _itemService.GetAllItems(true);
+		ItemCollectionView.ItemsSource = DisplayedItems;
 	}
 
 	private void OnSearchBarButtonPressed(object sender, EventArgs e)
 	{
-		var query = Search.Text;
-		SearchResultLabel.Text = $"Search query: {query}";
-
-		var items = _itemService.SearchForItems(query, true);
+		var items = _itemService.SearchForItems(Search.Text, true);
 	}
 
 	private void OnCreateNewClicked(object sender, EventArgs e)
